ca4ad7eb746ec096b9e5625d20c8fbca01ac1f36
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-08-13 09:46:36 -0500

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	lang: 'en-US',
+	title: 'Hello, VuePress!',
+	description: 'A VuePress site.',
+	base: '/website-rewrite/',
+
+	themeConfig: {
+		logo: 'https://vuejs.org/images/logo.png',
+	},
+}

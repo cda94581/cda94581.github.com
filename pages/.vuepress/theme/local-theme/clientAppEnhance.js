8c16d9ab540c2e3e541555ab4975620146365524
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-08-14 14:02:48 -0500

    Update File Paths for Organization

--- pages/.vuepress/theme/local-theme/clientAppEnhance.js
+++ pages/.vuepress/theme/local-theme/clientAppEnhance.js
@@ -1,8 +1,8 @@
 import { defineClientAppEnhance } from '@vuepress/client';
-import TwitchChannel from './Components/TwitchChannel.vue';
-import TwitterFollow from './Components/TwitterFollow.vue';
-import TwitterTimeline from './Components/TwitterTimeline.vue';
-import YouTubeVideo from './Components/YouTubeVideo.vue';
+import TwitchChannel from './Components/SocialEmbeds/TwitchChannel.vue';
+import TwitterFollow from './Components/SocialEmbeds/TwitterFollow.vue';
+import TwitterTimeline from './Components/SocialEmbeds/TwitterTimeline.vue';
+import YouTubeVideo from './Components/SocialEmbeds/YouTubeVideo.vue';
 import css from './Styles/css.css';
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {

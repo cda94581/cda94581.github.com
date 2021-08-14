174af9018043592f9941b6cec3916472e8d50746
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-08-14 14:15:27 -0500

    Move downloads

--- pages/.vuepress/theme/local-theme/index.js
+++
@@ -1,9 +0,0 @@
-const { path } = require('@vuepress/utils');
-
-const localTheme = {
-	name: 'local-theme',
-	extends: '@vuepress/theme-default',
-	clientAppEnhanceFiles: path.resolve(__dirname, './clientAppEnhance.js')
-}
-
-module.exports = localTheme;
